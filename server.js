2fcb4e634f91b34c50ce45284b804d4a72286372
1bryandeangelo <1bryandeangelo@gmail.com>
2026-01-23 20:08:46 -0500

    Change key

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const replicate = new Replicate({
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 app.get("/", (req, res) => {
